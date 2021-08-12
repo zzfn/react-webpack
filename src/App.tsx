4a9523506d7660f36ffa80eb1fb6ebcb16132a08
chenc <chenchen3@fosun.com>
2021-08-12 13:17:02 +0800

    feat: eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import {useState} from "react";
+import { useState } from 'react';
 
 function App() {
-    const [num, setNum] = useState(0)
-    return <div>
-        <button onClick={() => setNum(num + 1)}>+1</button>
-        <button onClick={() => setNum(num - 1)}>-1</button>
-        <hr/>
-        {num}
+  const [num, setNum] = useState(0);
+  return (
+    <div>
+      <button onClick={() => setNum(num + 1)}>+1</button>
+      <button onClick={() => setNum(num - 1)}>-1</button>
+      <hr />
+      {num}
     </div>
+  );
 }
 
-export {App}
+export { App };
